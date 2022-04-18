d8fc6830d2366d3dec18c2d1871e3424b28862e6
santt31 <santt31@gmail.com>
2022-04-19 01:33:46 +0200

    fix

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import './dist/style.css';
 import  Deck  from "./node_modules/siete-medio/clases/Deck.js";
 import  Hand  from "./node_modules/siete-medio/clases/Hand.js";
 
@@ -222,12 +223,4 @@ const results = () =>{
 /*Reset game*/
 document.getElementById("resetGame").addEventListener("click", () => {
   location.reload();
-});
-
-/*video */
-$(document).ready(function() {
-  $('#headerVideoLink').magnificPopup({
-    type:'inline',
-    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-  });         
 });
